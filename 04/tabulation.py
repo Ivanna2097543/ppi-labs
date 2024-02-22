3b50e972bc4b61e3c142708d9de435e0fc9a1e2e
Ivanna2097543 <ivannayaremko49@gmail.com>
2024-02-22 11:02:01 +0200

    lab4.1

--- 04/tabulation.py
+++ 04/tabulation.py
@@ -1,18 +1,18 @@
 from matplotlib import pyplot as plt
 
-def func(x: list[float]) -> list[float]:
+def func(x: list[float], N: float, a: float) -> list[float]:
     """
     Calculate function values for passed array of arguments
     """
-    return [ t/(t**2 + 1) for t in x ]
+    return [ N*(t/N + 0.5)**a - N/2 for t in x ]
 
-def tabulate(a: float, b: float, n: int) -> dict[float]:
+def tabulate(a: float, b: float, n: int, N: float, a_val: float) -> tuple[list[float], list[float]]:
     x = [ a + x*(b - a)/n for x in range(n) ]
-    y = func(x)
+    y = func(x, N, a_val)
     return (x, y)
 
 def main():
-    res = tabulate(0, 1, 1000)
+    res = tabulate(0, 1, 1000, 1, 2)
 
     plt.plot(res[0], res[1])
     plt.grid()
